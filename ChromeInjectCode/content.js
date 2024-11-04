f07a10e4ffab1ad249ae89ecc3afa01c58c59806
Hoda-Al-1 <github.host@hotmail.com>
2024-11-04 14:45:49 +0200

    check exact name

--- ChromeInjectCode/content.js
+++ ChromeInjectCode/content.js
@@ -11,11 +11,33 @@ var myInterval = setInterval(function () {
 		const cnt = parseInt(innerText.replaceAll('About', '').replaceAll('results', '').replaceAll(',', ''));
 		result.cnt = cnt;
 		if (cnt == 1) {
-			var _url = document.querySelector('[data-view-name="search-entity-result-universal-template"] a.app-aware-link').href;
-			_url = new URL(_url);
-			// Return a new object with the cleaned URL
-			_url = _url.origin + _url.pathname;
-			result.url = _url;
+			var nameSpan = document.querySelector('[data-view-name="search-entity-result-universal-template"] a.app-aware-link span[aria-hidden="true"]');
+			if (nameSpan) {
+				var foundName = nameSpan.innerText.toLocaleLowerCase();
+				// Get the query string from the current URL
+				const queryString = window.location.search;
+				// Create a URLSearchParams object from the query string
+				const urlParams = new URLSearchParams(queryString);
+				// Retrieve a specific parameter
+				const keywords = urlParams.get('keywords').toLocaleLowerCase();
+
+				//alert(keywords);
+				//alert(foundName);
+
+				if (keywords == foundName) {
+					var _url = document.querySelector('[data-view-name="search-entity-result-universal-template"] a.app-aware-link').href;
+					_url = new URL(_url);
+					if (!_url.pathname.includes("people")) {
+						// Return a new object with the cleaned URL
+						_url = _url.origin + _url.pathname;
+						result.url = _url;
+					} else {
+						result.cnt = 0;
+                    }
+				} else {
+					result.cnt = 0;
+				}
+			}
 		}
 	}
 
