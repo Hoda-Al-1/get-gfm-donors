5dbb7b67e3f4a9e703daa8ea5652d35fd581aeb4
Hoda-Al-1 <github.host@hotmail.com>
2024-12-23 17:06:38 +0200

    change

--- GoFundMeDonorsFetcherChromeExtension/general_scripts.js
+++ GoFundMeDonorsFetcherChromeExtension/general_scripts.js
@@ -15,7 +15,7 @@ var maxAmount = 0;
 var is_search_linkedin = true;
 var is_search_insta = true;
 var userLinledInWidowSearch = false;
-var delayMs = 15000;
+var delayMs = 5000;
 
 document.addEventListener('DOMContentLoaded', function () {
     console.info('DOM is fully loaded and parsed!');
