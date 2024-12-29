ccd22033abae697505b77cadd0f9744345651e07
Hoda-Al-1 <github.host@hotmail.com>
2024-12-29 15:05:46 +0200

    fix ajax

--- GoFundMeDonorsFetcherChromeExtension/search_linkedin.js
+++ GoFundMeDonorsFetcherChromeExtension/search_linkedin.js
@@ -34,11 +34,11 @@ async function get_linkedin_user(userFullName) {
 
 async function search_linkedin_user(search_keyword) {
     try {
-        var resp = await fetch("https://www.linkedin.com/voyager/api/graphql?variables=(start:0,origin:GLOBAL_SEARCH_HEADER,query:(keywords:" + search_keyword + ",flagshipSearchIntent:SEARCH_SRP,queryParameters:List((key:resultType,value:List(PEOPLE))),includeFiltersInResponse:false))&queryId=voyagerSearchDashClusters.cd5ee9d14d375bf9ca0596cfe0cbb926", {
+        var resp = await fetch("https://www.linkedin.com/voyager/api/graphql?variables=(start:0,origin:SWITCH_SEARCH_VERTICAL,query:(keywords:" + search_keyword + ",flagshipSearchIntent:SEARCH_SRP,queryParameters:List((key:resultType,value:List(PEOPLE))),includeFiltersInResponse:false))&queryId=voyagerSearchDashClusters.cd5ee9d14d375bf9ca0596cfe0cbb926", {
             "headers": {
                 "accept": "application/vnd.linkedin.normalized+json+2.1",
                 "accept-language": "en-US,en;q=0.9,ar;q=0.8",
-                "csrf-token": "ajax:5118546544084686507",
+                "csrf-token": "ajax:6796958486056805975",
                 "priority": "u=1, i",
                 "sec-ch-prefers-color-scheme": "light",
                 "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
@@ -48,12 +48,12 @@ async function search_linkedin_user(search_keyword) {
                 "sec-fetch-mode": "cors",
                 "sec-fetch-site": "same-origin",
                 "x-li-lang": "en_US",
-                "x-li-page-instance": "urn:li:page:d_flagship3_search_srp_people;CUH9MK9ARs+CaxK7vmy2wQ==",
+                "x-li-page-instance": "urn:li:page:d_flagship3_search_srp_people;qJTJp3FvTUKH/gLhbHJahQ==",
                 "x-li-pem-metadata": "Voyager - People SRP=search-results",
-                "x-li-track": "{\"clientVersion\":\"1.13.27831\",\"mpVersion\":\"1.13.27831\",\"osName\":\"web\",\"timezoneOffset\":2,\"timezone\":\"Africa/Cairo\",\"deviceFormFactor\":\"DESKTOP\",\"mpName\":\"voyager-web\",\"displayDensity\":1.25,\"displayWidth\":1920,\"displayHeight\":1080}",
+                "x-li-track": "{\"clientVersion\":\"1.13.28248.2\",\"mpVersion\":\"1.13.28248.2\",\"osName\":\"web\",\"timezoneOffset\":2,\"timezone\":\"Africa/Cairo\",\"deviceFormFactor\":\"DESKTOP\",\"mpName\":\"voyager-web\",\"displayDensity\":1.25,\"displayWidth\":1920,\"displayHeight\":1080}",
                 "x-restli-protocol-version": "2.0.0"
             },
-            "referrer": "https://www.linkedin.com/search/results/people/?keywords=" + search_keyword + "&origin=GLOBAL_SEARCH_HEADER&sid=X.f",
+            "referrer": "https://www.linkedin.com/search/results/all/?keywords=Fiona%20Cobain&origin=TYPEAHEAD_ESCAPE_HATCH&sid=Izu",
             "referrerPolicy": "strict-origin-when-cross-origin",
             "body": null,
             "method": "GET",
@@ -88,11 +88,11 @@ async function search_linkedin_user(search_keyword) {
 async function get_linkedin_profile_details(publicIdentifier) {
 
     try {
-        var resp = await fetch("https://www.linkedin.com/voyager/api/graphql?includeWebMetadata=true&variables=(vanityName:" + publicIdentifier + ")&queryId=voyagerIdentityDashProfiles.00bac58ea0526a82dce3cbe7ee8ddc6d", {
+        var resp = await fetch("https://www.linkedin.com/voyager/api/graphql?includeWebMetadata=true&variables=(vanityName:" + publicIdentifier + ")&queryId=voyagerIdentityDashProfiles.006f9921d016ba6bbed83dcb9e8bcab8", {
             "headers": {
                 "accept": "application/vnd.linkedin.normalized+json+2.1",
                 "accept-language": "en-US,en;q=0.9,ar;q=0.8",
-                "csrf-token": "ajax:5118546544084686507",
+                "csrf-token": "ajax:6796958486056805975",
                 "priority": "u=1, i",
                 "sec-ch-prefers-color-scheme": "light",
                 "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
@@ -102,12 +102,12 @@ async function get_linkedin_profile_details(publicIdentifier) {
                 "sec-fetch-mode": "cors",
                 "sec-fetch-site": "same-origin",
                 "x-li-lang": "en_US",
-                "x-li-page-instance": "urn:li:page:d_flagship3_profile_view_base;nmsE3qg7Q4+ZYVlIwwhmAw==",
+                "x-li-page-instance": "urn:li:page:d_flagship3_profile_view_base;d04gE2hiRKKb8vnDcWyKxQ==",
                 "x-li-pem-metadata": "Voyager - Profile=profile-top-card-supplementary",
-                "x-li-track": "{\"clientVersion\":\"1.13.27831\",\"mpVersion\":\"1.13.27831\",\"osName\":\"web\",\"timezoneOffset\":2,\"timezone\":\"Africa/Cairo\",\"deviceFormFactor\":\"DESKTOP\",\"mpName\":\"voyager-web\",\"displayDensity\":1.25,\"displayWidth\":1920,\"displayHeight\":1080}",
+                "x-li-track": "{\"clientVersion\":\"1.13.28248.2\",\"mpVersion\":\"1.13.28248.2\",\"osName\":\"web\",\"timezoneOffset\":2,\"timezone\":\"Africa/Cairo\",\"deviceFormFactor\":\"DESKTOP\",\"mpName\":\"voyager-web\",\"displayDensity\":1.25,\"displayWidth\":1920,\"displayHeight\":1080}",
                 "x-restli-protocol-version": "2.0.0"
             },
-            "referrer": "https://www.linkedin.com/in/adil-kortbi-5321bb1b2/",
+            "referrer": "https://www.linkedin.com/in/fiona-cobain-41b858145/",
             "referrerPolicy": "strict-origin-when-cross-origin",
             "body": null,
             "method": "GET",
